376f36cf1984e8eb10f72e98d3389a406c95131e
MiguelMao08 <109632976+MiguelMao08@users.noreply.github.com>
2022-09-22 18:13:32 -0400

    Add files via upload

--- bolaCanhao.js
+++ bolaCanhao.js
@@ -13,15 +13,17 @@ class bolaCanhao{
         
         image(bolaImg,this.body.position.x,this.body.position.y,this.r, this.r);
         
-        if(this.body.velocity.x>0 && this.body.position > 250){
+        if(this.body.velocity.x>0 && this.body.position.x > 250){
             //pegar a informação de posição x e y
-
+             
+          var posicao = [this.body.position.x,this.body.position.y];
             //colocar a informação na matriz trajetoria
-
+          this.trajetoria.push(posicao)
         }
         //percorrer a matriz trajetoria para mostrar cada ponto da trajetoria
         for(var i=0; i<this.trajetoria.length; i++){
             //mostrar a trajetoria usando o comando image
+            image(bolaImg,this.trajetoria[i][0],this.trajetoria[i][1],10,10)
         }
        
     }
@@ -30,7 +32,7 @@ class bolaCanhao{
         var novoAngulo=Canhao.angulo-28;
         novoAngulo=novoAngulo*(3.14/180);
         var velocidade=p5.Vector.fromAngle(novoAngulo);
-        velocidade.mult(0.2);
+        velocidade.mult(0.3);
         Matter.Body.setStatic(this.body,false);
         Matter.Body.setVelocity(this.body,{x:velocidade.x*(180/3.14),y:velocidade.y*(180/3.14)});
     }
